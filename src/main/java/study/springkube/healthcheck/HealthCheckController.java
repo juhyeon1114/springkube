5c66edf906828d9c1aa1508ffb6403bc3093f54e
juhyeon <juhyeon1114@gmail.com>
2024-12-25 22:19:29 +0900

    Probe 적용

---
+++ src/main/java/study/springkube/healthcheck/HealthCheckController.java
@@ -0,0 +1,31 @@
+package study.springkube.healthcheck;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/probe")
+public class HealthCheckController {
+
+    @GetMapping("/startup")
+    public String startup() {
+        log.info("startup");
+        return "OK";
+    }
+
+    @GetMapping("/ready")
+    public String ready() {
+        log.info("ready");
+        return "OK";
+    }
+
+    @GetMapping("/live")
+    public String live() {
+        log.info("live");
+        return "OK";
+    }
+
+}
