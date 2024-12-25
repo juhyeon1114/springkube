a4e587f4acc0e707f336285d12c4aa0bc372a39a
juhyeon <juhyeon1114@gmail.com>
2024-12-25 19:30:44 +0900

    init

---
+++ src/test/java/study/springkube/SpringkubeApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springkube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
