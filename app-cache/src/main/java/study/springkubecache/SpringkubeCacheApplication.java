a5995d7246b9c352f3a7e8875184fc4049b36fd7
juhyeon <juhyeon1114@gmail.com>
2024-12-27 22:52:37 +0900

    springkube cache 애플리케이션 보일러 플레이트

---
+++ app-cache/src/main/java/study/springkubecache/SpringkubeCacheApplication.java
@@ -0,0 +1,13 @@
+package study.springkubecache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkubeCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringkubeCacheApplication.class, args);
+    }
+
+}
