5096bb841c1f8f566500e1c06ac75dbca14ef511
juhyeon <juhyeon1114@gmail.com>
2025-01-09 20:29:01 +0900

    batch 애플리케이션 init

---
+++ app-batch/src/main/java/study/springkubebatch/SpringkubeBatchApplication.java
@@ -0,0 +1,13 @@
+package study.springkubebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkubeBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringkubeBatchApplication.class, args);
+    }
+
+}
