437133b16464cee7f00a39c316329800c1612606
juhyeon <juhyeon1114@gmail.com>
2025-01-17 16:08:14 +0900

    opensearch, fluentbit k8s 구성

--- app-opensearch/init.sh
+++ app-opensearch/init.sh
@@ -2,5 +2,7 @@
 
 helm repo add opensearch https://opensearch-project.github.io/helm-charts
 kubectl create namespace opensearch
-helm -n opensearch install opensearch opensearch/opensearch
+helm -n opensearch install opensearch opensearch/opensearch --values=values.yaml
 helm -n opensearch install dashboard opensearch/opensearch-dashboards
+
+kubectl -n opensearch port-forward dashboard-opensearch-dashboards-675fd5df99-2qsnj 5601
